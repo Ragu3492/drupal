3a1a2ccb75e681d063fa14d4d21f14882310cb7d
1996karthick <61176771+1996karthick@users.noreply.github.com>
2020-04-22 14:00:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,11 @@ sudo systemctl restart httpd
 sudo yum -y install mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
 
+sudo echo "CREATE DATABASE zippyops_db CHARACTER SET utf8 COLLATE utf8_general_ci;;" | mysql
+sudo echo "CREATE USER 'zippyops'@'localhost' IDENTIFIED BY 'zippyops';" | mysql
+sudo echo "GRANT ALL PRIVILEGES ON zippyops_db.* TO 'zippyops'@'localhost';" | mysql
+sudo echo "FLUSH PRIVILEGES;" | mysql
+
 cd /var/www/html/
 sudo yum install drush -y
 sudo drush dl drupal-8
